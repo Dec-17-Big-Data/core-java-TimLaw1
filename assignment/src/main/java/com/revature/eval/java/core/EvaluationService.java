f5673141318618cfd692c4b5cc38919f7b5cf270
TimLaw1 <tlaw1122@gmail.com>
2018-12-19 19:39:46 -0500

    Found a solution to question 3
    Simple question, number 3. Not hard to solve.

--- assignment/src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ assignment/src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -100,17 +100,17 @@ public class EvaluationService {
 
 		public boolean isEquilateral() {
 			// TODO Write an implementation for this method declaration
-			return false;
+			return ((this.sideOne==this.sideTwo)&&(this.sideOne==this.sideThree));
 		}
 
 		public boolean isIsosceles() {
 			// TODO Write an implementation for this method declaration
-			return false;
+			return (((this.sideOne==this.sideTwo)&&(this.sideOne!=this.sideThree))||((this.sideOne==this.sideThree)&&(this.sideOne!=this.sideTwo))||(this.sideTwo==this.sideThree)&&(this.sideOne!=this.sideTwo));
 		}
 
 		public boolean isScalene() {
 			// TODO Write an implementation for this method declaration
-			return false;
+			return ((this.sideOne!=this.sideTwo)&&(this.sideTwo!=this.sideThree)&&(this.sideOne!=this.sideThree));
 		}
 
 	}
